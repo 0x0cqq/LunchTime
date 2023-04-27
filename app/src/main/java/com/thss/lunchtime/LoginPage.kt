1de6468a0d61ddff16fd849417c11f400a350f9b
ChenQiqian <cqqqwq@outlook.com>
2023-04-28 02:35:59 +0800

    feat: add LoginPage

---
+++ app/src/main/java/com/thss/lunchtime/LoginPage.kt
@@ -0,0 +1,146 @@
+package com.thss.lunchtime
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun LoginPage(onClickLogin : (username: String, password: String) -> Unit,
+              onClickSignup: (username: String, password: String) -> Unit) {
+    BoxWithConstraints(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize().imePadding().imeNestedScroll(),
+    ) {
+        if (maxWidth > maxHeight) { // 横屏
+            Row (
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                LoginTitle()
+                LoginPanel(onClickLogin, onClickSignup)
+            }
+        } else { //竖屏
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                LoginTitle()
+                LoginPanel(onClickLogin, onClickSignup)
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginTitle(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.title),
+        contentDescription = "Title Image",
+        modifier = modifier.fillMaxWidth(0.5f)
+    )
+}
+
+
+@Composable
+fun LoginPanel(onClickLogin : (username: String, password: String) -> Unit,
+              onClickSignup: (username: String, password: String) -> Unit) {
+    var username by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // 用户名栏
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            leadingIcon = {
+                Icon(Icons.Outlined.Person,"Username")
+            },
+            singleLine = true,
+            keyboardActions = KeyboardActions {
+                focusManager.moveFocus(FocusDirection.Next)
+            },
+        )
+        // 密码输入栏
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation =
+                if (passwordVisible) VisualTransformation.None
+                else PasswordVisualTransformation(),
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            leadingIcon = {
+                Icon(Icons.Outlined.Lock, "Password")
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            keyboardActions = KeyboardActions {
+                focusManager.clearFocus()
+            },
+            trailingIcon = {
+                val image = if (passwordVisible)
+                    Icons.Filled.Visibility
+                else Icons.Filled.VisibilityOff
+
+                val description = if (passwordVisible) "Hide password" else "Show password"
+
+                IconButton(onClick = { passwordVisible = !passwordVisible }){
+                    Icon(imageVector = image, description)
+                }
+            }
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            // 登录和注册按钮，运用
+            Button(onClick = { onClickLogin(username, password) }, modifier = Modifier
+                .padding(10.dp)
+                .weight(1f)) {
+                Text("Log In")
+            }
+            Button(onClick = { onClickSignup(username, password) }, modifier = Modifier
+                .padding(10.dp)
+                .weight(1f)) {
+                Text("Sign Up")
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginPagePreview() {
+    LoginPage(onClickLogin = { _, _ -> }, onClickSignup = { _, _ -> })
+}
--- app/src/main/java/com/thss/lunchtime/MainActivity.kt
+++ app/src/main/java/com/thss/lunchtime/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.thss.lunchtime
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,10 +12,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.thss.lunchtime.ui.theme.LunchTimeTheme
 
+@RequiresApi(Build.VERSION_CODES.R)
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+//        } else {
+//            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+//        }
         super.onCreate(savedInstanceState)
         setContent {
             LunchTimeTheme {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginPage({ _, _ -> }, { _, _ -> })
                 }
             }
         }
