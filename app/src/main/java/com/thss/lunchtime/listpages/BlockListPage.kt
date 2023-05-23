16688d769ef396d1d7c8d81e18cb86957b8ecc9a
chrisT-T <christychengyy@qq.com>
2023-05-23 20:04:18 +0800

    feat: fansListPage, BlockListPage

---
+++ app/src/main/java/com/thss/lunchtime/listpages/BlockListPage.kt
@@ -0,0 +1,82 @@
+package com.thss.lunchtime.listpages
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.thss.lunchtime.component.InfoData
+import com.thss.lunchtime.component.InfoPreviewComp
+import com.thss.lunchtime.mainscreen.infopage.ImageChange
+import com.thss.lunchtime.mainscreen.infopage.InfoEditPage
+import com.thss.lunchtime.mainscreen.infopage.InfoEditViewModel
+import com.thss.lunchtime.mainscreen.infopage.SimpleInfoChange
+import com.thss.lunchtime.post.PostData
+import com.thss.lunchtime.post.PostReviewCard
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@Composable
+fun BlockListPage(followingList: List<InfoData>) {
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+
+    }
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            imageVector = Icons.Default.ArrowBack,
+                            contentDescription = "back"
+                        )
+                    }
+                },
+                title = {
+                    Text(text = "黑名单列表")
+                }
+            )
+        }
+    ) { paddingValues -> Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues)
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            LazyColumn(modifier = Modifier.fillMaxSize()
+            ) {
+                items(followingList) { infoData ->
+                    InfoPreviewComp(msg = infoData)
+                }
+            }
+        }
+    }
+    }
+}
+
+val BlockArray = listOf(
+    InfoData(relation = 3),
+    InfoData(relation = 3),
+    InfoData(relation = 3),
+    InfoData(relation = 3),
+    InfoData(relation = 3),
+)
+
+@Preview
+@Composable
+fun BlockPreview() {
+    BlockListPage (followingList = BlockArray)
+}
