4a43ce2d4f6edf9ea597f18884ca36558a2057ea
chrisT-T <christychengyy@qq.com>
2023-05-24 01:50:39 +0800

    feat: searchPage

--- app/src/main/java/com/thss/lunchtime/search/SearchPage.kt
+++ app/src/main/java/com/thss/lunchtime/search/SearchPage.kt
@@ -1,8 +1,11 @@
 package com.thss.lunchtime.search
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.rounded.ArrowBack
@@ -10,6 +13,8 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.thss.lunchtime.mainscreen.infopage.postArray
+import com.thss.lunchtime.post.PostReviewCard
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,7 +26,12 @@ fun SearchPage()
         paddingValues ->  Column(modifier = Modifier
         .fillMaxWidth()
         .padding(paddingValues)) {
-
+        LazyColumn(modifier = Modifier.fillMaxSize()
+        ) {
+            items(postArray) { postData ->
+                PostReviewCard({}, {}, msg = postData)
+            }
+        }
     }
     }
 }
