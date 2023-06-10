52f6187a7c49caf3da8bf57181ff8511a400afb3
SaltyFish-er <1742412922@qq.com>
2023-06-10 20:46:25 +0800

    feat: switch to homepage when click notification

--- app/src/main/java/com/thss/lunchtime/component/NotificationShow.kt
+++ app/src/main/java/com/thss/lunchtime/component/NotificationShow.kt
@@ -3,21 +3,30 @@ package com.thss.lunchtime.component
 import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.thss.lunchtime.R
 import android.content.Context
+import android.content.Intent
+import com.thss.lunchtime.MainActivity
 
 const val CHANNEL_ID = "lunchTime"
 fun showNotification(context: Context, title: String, content: String) {
+
+    val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
 
     val builder = NotificationCompat.Builder(context, CHANNEL_ID)
         .setSmallIcon(R.drawable.forumicon)
         .setContentTitle(title)
         .setContentText(content)
         .setPriority(NotificationCompat.PRIORITY_MAX)
+        .setContentIntent(pendingIntent)
         .setAutoCancel(true)
 
     with(NotificationManagerCompat.from(context)) {
