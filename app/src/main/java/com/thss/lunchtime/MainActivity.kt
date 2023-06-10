f2f6bc48c130906f3397ceb4b8f44d8fbf2be5cf
SaltyFish-er <1742412922@qq.com>
2023-06-10 21:24:20 +0800

    fix: sometimes failed when register and login

--- app/src/main/java/com/thss/lunchtime/MainActivity.kt
+++ app/src/main/java/com/thss/lunchtime/MainActivity.kt
@@ -267,12 +267,6 @@ fun Application(modifier: Modifier = Modifier) {
                             ).show()
                             delay(1000)
                             if( response.status ) {
-                                LunchTimeNotificationService.connect(
-                                    userName = state.name,
-                                    onReceive = { notice ->
-                                        showNotification(context, notice.userName, notice.content)
-                                    }
-                                )
                                 applicationNavController.navigate(
                                     "main",
                                     NavOptions.Builder().setPopUpTo("login", true).build()
@@ -280,6 +274,12 @@ fun Application(modifier: Modifier = Modifier) {
                                 userData.updateData { userData ->
                                     userData.toBuilder().setUserName(state.name).setIsLogin(true).build()
                                 }
+                                LunchTimeNotificationService.connect(
+                                    userName = state.name,
+                                    onReceive = { notice ->
+                                        showNotification(context, notice.userName, notice.content)
+                                    }
+                                )
                             }
                         } catch ( e : Exception) {
                             Log.e("LunchTime", e.toString())
