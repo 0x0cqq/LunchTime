c17ba35aa3a7526d3c4b28399732a3f0fcf33a1b
ChenQiqian <cqqqwq@outlook.com>
2023-06-10 19:44:13 +0800

    fix: fix image player

--- app/src/main/java/com/thss/lunchtime/mediaplayer/ImagePlayer.kt
+++ app/src/main/java/com/thss/lunchtime/mediaplayer/ImagePlayer.kt
@@ -1,33 +1,58 @@
 package com.thss.lunchtime.mediaplayer
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.origeek.imageViewer.viewer.ImageViewer
+import com.origeek.imageViewer.viewer.rememberViewerState
+import kotlinx.coroutines.launch
 
 @Composable
 fun ImagePlayPage(url: String, onBack: () -> Unit) {
+    val scope = rememberCoroutineScope()
+    val state = rememberViewerState()
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.surface)
-            .clickable { onBack() },
+            .background(Color.Black),
         contentAlignment = Alignment.Center,
     ) {
-        AsyncImage(
-            model = url.toUri(),
-            contentDescription = "image",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier,
-            alignment = Alignment.Center
+        ImageViewer(
+            state = state,
+            model = rememberCoilImagePainter(url.toUri()),
+            modifier = Modifier.fillMaxSize(),
+            detectGesture = {
+                onTap = {
+                    onBack()
+                }
+                onDoubleTap = {
+                    scope.launch {
+                        state.toggleScale(it)
+                    }
+                }
+            },
         )
     }
+}
+
+@Composable
+fun rememberCoilImagePainter(image: Any): Painter {
+    // 加载图片
+    val imageRequest = ImageRequest.Builder(LocalContext.current)
+        .data(image)
+        .size(coil.size.Size.ORIGINAL)
+        .build()
+    // 获取图片的初始大小
+    return rememberAsyncImagePainter(imageRequest)
 }
